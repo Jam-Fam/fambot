5636765fb66fcd62d734ceda7dae9ad678e218d9
Nathan Wiles <ntwiles@gmail.com>
2020-08-27 11:52:28 -0400

    Staging info.

--- commands/info.js
+++ commands/info.js
@@ -1,14 +1,27 @@
 const Discord = require('discord.js');
 
+const maintainers = [
+  { 
+    username: 'ntwiles',
+    id: '222194207710773249'
+  }, 
+  { 
+    username: 'Marlo',
+    id: '292593191348076544'
+  }
+];
+
 module.exports = {
     name: 'info',
     description: "Gives info about the bot.",
 
     execute(msg, args) {     
 
+      let maintainerStr = maintainers.map(m => `<@${m.id}>`).join(', ');
+
       let embed = new Discord.RichEmbed()
         .setTitle("FamBot Info")
-        .addField('Maintained by', "@ntwiles", true)
+        .addField('Maintained by', maintainerStr, true)
         .addField('GitHub',"[click here](https://github.com/ntwiles/jamfam-fambot)", true);
       msg.channel.send(embed);
     },
