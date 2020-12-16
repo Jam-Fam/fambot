e2e9d412965f27c7a85a23f0f52e5c1069391d38
Nathan Wiles <ntwiles@gmail.com>
2020-12-15 22:46:07 -0500

    General cleanup.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import commands from './commands.js'
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 
+// TODO Can we just commands.map here? 
 Object.keys(commands).map(key => {
   bot.commands.set(commands[key].name, commands[key]);
 });
@@ -18,19 +19,19 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
   if (!msg.content.startsWith(config.PREFIX)) return;
+
   msg.content = msg.content.substring(config.PREFIX.length);
 
   const args = msg.content.split(/ +/);
   const command = args.shift().toLowerCase();
+
   console.info(`Called command: ${command}`);
 
   if (!bot.commands.has(command)) return;
 
-  try {
-    bot.commands.get(command).execute(msg, args);
-  } 
+  try { bot.commands.get(command).execute(msg, args); } 
   catch (error) {
     console.error(error);
-    msg.reply('there was an error trying to execute that command!');
+    msg.reply('There was an error trying to execute that command!');
   }
 });
